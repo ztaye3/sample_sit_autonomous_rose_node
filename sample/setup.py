92cd68b41adc03f7840841ccebfbb5a709f6662d
ztaye3 <82846776+ztaye3@users.noreply.github.com>
2021-09-04 09:05:44 +0200

    Update setup.py

--- sample/setup.py
+++ sample/setup.py
@@ -5,7 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
     packages=['sample'],
-    package_dir={'': 'src'}
+    package_dir={'': 'scripts'}
 )
 
 setup(**setup_args)
+
