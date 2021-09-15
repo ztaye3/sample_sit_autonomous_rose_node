27522518c738dac3ff34eefd182d34bdbb1b5752
ztaye3 <82846776+ztaye3@users.noreply.github.com>
2021-09-15 23:24:56 +0200

    Update compile.py

--- sample/scripts/compile.py
+++ sample/scripts/compile.py
@@ -1,11 +1,6 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-ext_modules = [
-    Extension("mymodule1",  ["talker.py"]),
-]
+from setuptools import setup
+from Cython.Build import cythonize
+
 setup(
-    name = 'My Program Name',
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = ext_modules
+    ext_modules = cythonize("test.pyx")
 )
