af6ba087ce1ac88d54e0f150ff9c560b8a208ec3
ztaye3 <82846776+ztaye3@users.noreply.github.com>
2021-09-16 01:07:47 +0200

    Update setup.py

--- sample/setup.py
+++ sample/setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
     packages=[''],
-    package_dir={'': 'scripts'}
+    package_dir={'': 'scripts'},
+    requires=['std_msgs', 'rospy']
 )
 
 setup(**setup_args)
